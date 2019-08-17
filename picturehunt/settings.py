d69b1e7a69d60fd0b4d4bf2c551e1e7313dc6487
Robert Claus <robertclaus@gmail.com>
2019-08-17 05:37:20 -0500

    Try to fix image urls

--- picturehunt/settings.py
+++ picturehunt/settings.py
@@ -117,5 +117,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..','www','media')
+
 django_heroku.settings(locals())
 
--- picturehunt/urls.py
+++ picturehunt/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from django.contrib import admin
@@ -23,3 +25,5 @@ urlpatterns = [
     path("login/", picturehunt.views.login, name="login"),
     path("logout/", picturehunt.views.logout, name="logout"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
